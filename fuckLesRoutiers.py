2897d4f4da7025dd1be0ed1da9cc9a05922a3518
RemiFeydit <remi.feydit@ynov.com>
2019-10-22 12:46:25 +0200

    Fin partie 1

--- fuckLesRoutiers.py
+++ fuckLesRoutiers.py
@@ -1,16 +1,35 @@
 from dictionnaire import villes
 
 def salut(start, end):
-    maxSpeed = 90/3.6
+    ## Gestion de la casse
     start = start.lower().capitalize()
     end = end.lower().capitalize()
-    distance = villes[start][end] / 1000
-    maxTime = distance / maxSpeed
-    # heures = int((distance / maxSpeed))
-    # minutes = int(60 * (round((distance / maxSpeed), 2) % 1))
-    # return "Le temps nécessaire pour faire {} - {} est de {} heures {} minutes.".format(start, end, heures, minutes)
+    timeDuringAccAndDecc = 18
+    timeOutTime = 15
+    ## Conversion en km/h à m/s
+    maxSpeed = 90/3.6
+    ## Conversion de kilomètres à mètres
+    distance = villes[start][end] * 1000
+    ## Temps si la vitesse et constante et sans pause
+    maxTime = (distance / maxSpeed) / 60
+    ## Nombre de pauses
+    nbPause = (maxTime // 60) // 2
+    ## Distance parcourue sans la distance parcourus avec la décelération et l'accélération
+    distance = distance - (timeOut() * nbPause)
+    ## Temps mis sans les pauses en minutes
+    timeWithoutPause = (distance / maxSpeed) / 60
+    ## 
+    time =  timeWithoutPause + (timeDuringAccAndDecc + timeOutTime) * nbPause
+    ## Conversion temps en heure pleines
+    heures = int(time/60)
+    minutes = int(round(((time/60) % 1),2) * 60)
+    return "Le temps nécessaire pour faire {} - {} est de {} heures {} minutes.".format(start, end, heures, minutes)
 
-
+def timeOut():
+    averageSpeed = 50/3.6
+    time = 18*60
+    distanceTravelled = averageSpeed * time
+    return distanceTravelled
 
 print("Entrez la ville de départ : ")
 ntm = input()
